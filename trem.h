207fb5473843925a3f1e91c63437e64de70feaf4
dinizjunioo <102337092+dinizjunioo@users.noreply.github.com>
2022-11-18 14:50:54 -0300

    Add files via upload

---
+++ trem.h
@@ -0,0 +1,44 @@
+#ifndef TREM_H
+#define TREM_H
+
+#include <QThread>
+
+/*
+ * Classe Trem herda QThread
+ * Classe Trem passa a ser uma thread.
+ * A função START inicializa a thread. Após inicializada, a thread irá executar a função RUN.
+ * Para parar a execução da função RUN da thread, basta executar a função TERMINATE.
+ *
+*/
+class Trem: public QThread{
+ Q_OBJECT
+public:
+
+    Trem(int,int,int);  //construtor
+    void run();         //função a ser executada pela thread
+
+    // funções que recebem o valor atual do slide e assim controlam a velocidade de cada trem
+    void get_velo1(int value);
+    void get_velo2(int value);
+    void get_velo3(int value);
+    void get_velo4(int value);
+    void get_velo5(int value);
+
+//Cria um sinal
+signals:
+    void updateGUI(int,int,int);
+
+private:
+   int x;           //posição X do trem na tela
+   int y;           //posição Y do trem na tela
+   int ID;          //ID do trem
+
+   //criar uma struct depois para isso aqui
+   int velocidade1;  //Velocidade. É o tempo de dormir em milisegundos entre a mudança de posição do trem
+   int velocidade2;
+   int velocidade3;
+   int velocidade4;
+   int velocidade5;
+};
+
+#endif // TREM_H
